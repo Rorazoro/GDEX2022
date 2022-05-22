b01a8bf083c00f3d8e4b8fed95e04312ff943cd8
Rorazoro <rorazoro@gmail.com>
2022-05-22 16:54:45 -0400

    Changes from feedback plus more level modules

---
+++ Assets/_Project/Scripts/Gameplay/SpinningBeam.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Gameplay
+{
+    public class SpinningBeam : MonoBehaviour
+    {
+        public float speed;
+        private void Update()
+        {
+            transform.Rotate(Vector3.right * ( speed * Time.deltaTime ));
+        }
+    }
+}
